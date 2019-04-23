7263f15520fa3e275c704a33caf7eaa8ca8d6b95
jessanderson222 <jessanderson222@gmail.com>
2019-04-23 12:56:36 -0400

    Serializers set up

--- app/serializers/recommendation_serializer.rb
+++ app/serializers/recommendation_serializer.rb
@@ -1,3 +1,7 @@
 class RecommendedSerializer < ActiveModel::Serializer
-  attributes :id
+
+  attributes :id, :budget, :saving, :food, :utility, :housing, :transportation, :health, :insurance, :recreation, :personal, :misc
+  belongs_to :guideline
+  belongs_to :user
+
 end
