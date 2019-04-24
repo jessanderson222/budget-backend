8d1b29533985363fcd9868c37d4c2858304f5429
jessanderson222 <jessanderson222@gmail.com>
2019-04-24 14:16:25 -0400

    Guideline controller includes index and show with private parameters

--- app/controllers/api/v1/guidelines_controller.rb
+++ app/controllers/api/v1/guidelines_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::GuidelinesController < ApplicationController
+
+def index
+    @guidelines = Guideline.all 
+    render(json: @guidelines)
+end 
+
+def show
+    @guideline = Guideline.find(params[:id])
+    render json: @guideline, status: 200
+end 
+
+private 
+def guideline_params
+    params.require(:guideline).permit(:id, :saving, :food, :utility, :housing, :transportation, :health, :insurance, :recreation, :personal, :misc)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do 
+    namespace :v1 do 
+      resources :guidelines
+      resources :users
+      resources :recommendations
+    end 
+  end 
 end
