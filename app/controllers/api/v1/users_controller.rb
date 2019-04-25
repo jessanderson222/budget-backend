bce38ee9d12513f636e25816546bd89991fa7b83
jessanderson222 <jessanderson222@gmail.com>
2019-04-25 17:38:12 -0400

    Users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::UsersController < ApplicationController
+    def index
+        @users = User.all 
+        render(json: @users)
+    end 
+
+    def show 
+        @user = User.find(params[:id])
+        render json: @user, status: 200
+    end 
+    
+    def create
+        @user = User.create(user_params)
+        render json: @user, status: 201
+    end 
+
+    private 
+    def user_params
+        params.require(:user).permit(:id, :budget, :saving, :food, :utility, :housing, :transportation, :health, :insurance, :recreation, :personal, :misc)
+    end 
 end
