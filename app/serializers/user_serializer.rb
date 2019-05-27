7ee93c0239c59881572372f0752d043fbbf7241b
jessanderson222 <jessanderson222@gmail.com>
2019-05-27 09:56:55 -0600

    username, email, and img_url in serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :budget, :saving, :food, :utility, :housing, :transportation, :health, :insurance, :recreation, :personal, :misc
+  attributes :id, :username, :email, :img_url, :budget, :saving, :food, :utility, :housing, :transportation, :health, :insurance, :recreation, :personal, :misc
   has_many :recommendations
 end
